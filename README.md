8b5b99f6cd6b70ef7aacfca610db4c5af26ffbb2
Roman <romanbelaire@csu.fullerton.edu>
2019-04-19 23:59:38 -0700

    Adding cascade classifiers

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ParkingBuddy
+cas1.xml from https://github.com/abhi-kumar/CAR-DETECTION
--- parking.py
+++ parking.py
@@ -4,7 +4,7 @@ import cv2
 
 
 camera = cv2.VideoCapture(1)
-car_cascade = cv2.CascadeClassifier('cars.xml')
+car_cascade = cv2.CascadeClassifier('cas1.xml')
 
 def getImage():
     #camera.open()
@@ -15,13 +15,13 @@ def getImage():
 
 def detectCars(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cars, numCars = car_cascade.detectMultiScale2(gray)
+    cars, numCars = car_cascade.detectMultiScale2(gray, minNeighbors = 1, minSize = (30, 30), maxSize = (60, 60))
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
     print(len(cars))
     cv2.imshow(str(numCars), img)
     cv2.waitKey(0)
 
-image = cv2.imread('dataset/freeway.jpeg')
-image = getImage()
+image = cv2.imread('dataset/parking2.jpg')
+#image = getImage()
 detectCars(image)
